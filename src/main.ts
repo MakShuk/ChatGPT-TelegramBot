156e02c0ba9910455c5718516093589c7868c6ca
Maksim Shuklin <makshuk@my.com>
2023-05-08 22:17:45 +0300

    add voise message

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { Telegraf } from 'telegraf';
+import { message } from 'telegraf/filters';
+import config from 'config';
+import { link } from 'fs';
+
+const bot = new Telegraf(config.get('TELEGRAM_TOKEN'));
+
+bot.on(message('voice'), async (context) => {
+	try {
+		const userId = context.message.from.id;
+		//await context.reply(JSON.stringify(context.message.voice, null, 2));
+		const link = await context.telegram.getFileLink(context.message.voice.file_id);
+		await context.reply(JSON.stringify(link, null, 2));
+	} catch (e: any) {
+		console.log('Error while voice message', e.message);
+	}
+});
+
+bot.command('start', async (context) => {
+	await context.reply(JSON.stringify(context.message, null, 2));
+});
+
+bot.launch();
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
---
+++ src/ogg.ts
@@ -0,0 +1,7 @@
+class OggConvector {
+	constructor() {}
+	toMp3() {}
+	create() {}
+}
+
+export const ogg = new OggConvector();
