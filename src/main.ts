7bde4f569dc9d488e1d9b78961168eb539a3afe2
Maksim Shuklin <makshuk@my.com>
2023-05-09 09:18:28 +0300

    add create  ogg metod

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { Telegraf } from 'telegraf';
 import { message } from 'telegraf/filters';
 import config from 'config';
-import { link } from 'fs';
+import { ogg } from './ogg.js';
 
 const bot = new Telegraf(config.get('TELEGRAM_TOKEN'));
 
 bot.on(message('voice'), async (context) => {
 	try {
 		const userId = context.message.from.id;
-		//await context.reply(JSON.stringify(context.message.voice, null, 2));
+		await context.reply(JSON.stringify(context.message.voice, null, 2));
 		const link = await context.telegram.getFileLink(context.message.voice.file_id);
 		await context.reply(JSON.stringify(link, null, 2));
 	} catch (e: any) {
--- src/ogg.ts
+++ src/ogg.ts
@@ -1,7 +1,29 @@
+import axios from 'axios';
+import { createWriteStream } from 'fs';
+import { dirname, resolve } from 'path';
+
+//const __dirname = dirname(fileURLToPath(import.meta.url));
+
 class OggConvector {
-	constructor() {}
-	toMp3() {}
-	create() {}
+	//constructor() {}
+	//toMp3() {}
+	async create(url: string, filename: string): Promise<void> {
+		try {
+			const oggPath = resolve(__dirname, '../voices', `${filename}.ogg`);
+			const response = await axios({
+				method: 'get',
+				url,
+				responseType: 'stream',
+			});
+			return new Promise((resolve) => {
+				const stream = createWriteStream(oggPath);
+				response.data.pipe(stream);
+				stream.on('finish', () => resolve(oggPath));
+			});
+		} catch (e: any) {
+			console.log(e.message);
+		}
+	}
 }
 
 export const ogg = new OggConvector();
