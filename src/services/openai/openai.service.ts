21d9f90ee575ffce1faff6656b639633784c8b16
Maksim Shuklin <makshuk@my.com>
2023-05-10 08:23:30 +0300

    saved

--- src/services/openai/openai.service.ts
+++ src/services/openai/openai.service.ts
@@ -1,3 +1,14 @@
-export class OpenAi {
+import { Configuration, OpenAIApi } from 'openai';
+import config from 'config';
 
+export class OpenAi {
+	private configuration: Configuration;
+	private openai: OpenAIApi;
+
+	constructor(apiKey: string) {
+		this.configuration = new Configuration({
+			apiKey,
+		});
+		this.openai = new OpenAIApi(this.configuration);
+	}
 }
